493d50b49e60bb70e79d51bead2b024dce90a609
silverun <imonutiy@gmail.com>
2021-07-13 23:03:16 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+export const KEY = '6c10723d-7d3e-4d83-9bdf-e5105bd84dc8';
+export const MODAL_CLOSE_SEC = 2.5;
